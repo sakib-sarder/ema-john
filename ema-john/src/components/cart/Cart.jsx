a55e0d54aa066b18da4b7a106599e840318a7b80
sakib <s.sarder232002@gmail.com>
2023-03-29 22:48:58 +0600

    last module done

--- ema-john/src/components/cart/Cart.jsx
+++ ema-john/src/components/cart/Cart.jsx
@@ -3,15 +3,21 @@ import './Cart.css';
 const Cart = ({ cart }) => {
     let total = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for (const product of cart) {
-        total = total + product.price;
+        // if (product.quantity === 0) {
+        //     product.quantity = 1;
+        // }
+        // product.quantity = product.quantity || 1;
+        total = total + product.price *product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = parseFloat((total * 0.07).toFixed(2));
     return (
         <div className="cart">
             <h3>Order Summary</h3>
-            <p>Selected Items:{cart.length}</p>
+            <p>Selected Items:{quantity}</p>
             <p>Total Price:${total}</p>
             <p>Total Shipping:{totalShipping}</p>
             <p>Tax: ${tax}</p>
--- ema-john/src/components/shop/Shop.jsx
+++ ema-john/src/components/shop/Shop.jsx
@@ -23,12 +23,26 @@ const Shop = () => {
         addedProduct.quantity = quantity;
         savedCart.push(addedProduct)
       }
-      setCart(savedCart)
+      // console.log(addedProduct);
     }
+    setCart(savedCart);
   },[products])
 
   const handleAddToCart = (product) => {
-    const newCart = [...cart, product];
+    // const newCart = [...cart, product];
+
+    let newCart = [];
+    const exists = cart.find(pd => pd.id === product.id);
+    console.log(exists);
+    if (!exists) {
+      product.quantity = 1;
+      newCart = [...cart, product];
+    } else {
+      exists.quantity = exists.quantity + 1;
+      const remaining = cart.filter(pd => pd.id !== product.id);
+      newCart = [...remaining, exists];
+    }
+
     setCart(newCart);
     addToDb(product.id)
   };
